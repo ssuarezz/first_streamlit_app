5f6294d712d4c55efdeae23f7797330fa03c19ea
ssuarezz <sergiosuazap@gmail.com>
2022-12-01 16:02:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error impor URLError
+from urllib.error import URLError
 
 
 streamlit.title('My parents new healthy dinner!')
